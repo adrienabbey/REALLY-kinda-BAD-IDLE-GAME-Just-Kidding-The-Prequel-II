d0fbe75c7cdee48083d21238def60671ae045c1d
rabiddoughnuts <111888263+rabiddoughnuts@users.noreply.github.com>
2024-01-22 13:26:33 -0500

    Finished character creation, starting world next, fuck Load

--- src/StartScreen.java
+++ src/StartScreen.java
@@ -1,4 +1,6 @@
 import java.awt.Dimension;
+import java.io.IOException;
+
 import javax.swing.*;
 // TODO Narrow the swing down to what we actually need
 
@@ -23,7 +25,12 @@ class StartScreen extends JFrame{
         quit.setAlignmentX(CENTER_ALIGNMENT);
 
         newGame.addActionListener(e -> {
-            new CharacterCreation();
+            try {
+                new CharacterCreation();
+            } catch (IOException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            }
             this.dispose();
         });
         loadGame.addActionListener(e -> {
@@ -35,7 +42,7 @@ class StartScreen extends JFrame{
         });
 
         this.getContentPane().add(start);
-        this.setSize(300, 300);
+        this.setSize(900, 600);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
     }
--- src/World.java
+++ src/World.java
@@ -1,3 +1,7 @@
-class World{
-    
+import javax.swing.*;
+
+class World extends JFrame{
+    public World(PlayerCharacter player){
+        System.out.println("Hello World");
+    }
 }
