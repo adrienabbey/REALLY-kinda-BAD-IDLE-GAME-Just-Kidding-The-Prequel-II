056c50bf1f506e69831cda5f9899e9378b57653b
rabiddoughnuts <111888263+rabiddoughnuts@users.noreply.github.com>
2024-01-21 21:52:55 -0500

    Start? I haven't even finished yet

--- docs/journals/BrandonsJournal.md
+++ docs/journals/BrandonsJournal.md
@@ -2,3 +2,6 @@
 
 Filled in the attack function, function for drinking potions. Made a basic gear class and some assorted fields to the player character. Added classes for all of the classes declared on the class diagram.
 
+Making a garbage GUI to test functionality as its added.
+
+Made my really terribly start screen.
--- src/StartScreen.java
+++ src/StartScreen.java
@@ -1,5 +1,42 @@
-class StartScreen{
+import java.awt.Dimension;
+import javax.swing.*;
+// TODO Narrow the swing down to what we actually need
+
+class StartScreen extends JFrame{
     public StartScreen(){
-        
+        // TODO - Add a info button or something to explain the game
+        JPanel start = new JPanel(); // Start panel where you select new game, load, or quit
+        start.setLayout(new BoxLayout(start, BoxLayout.Y_AXIS));
+        JButton newGame = new JButton("New Game");
+        JButton loadGame = new JButton("Load Game");
+        JButton quit = new JButton("Quit");
+        start.add(Box.createVerticalGlue());
+        start.add(newGame);
+        start.add(Box.createRigidArea(new Dimension(0, 10)));
+        start.add(loadGame);
+        start.add(Box.createRigidArea(new Dimension(0, 10)));
+        start.add(quit);
+        start.add(Box.createVerticalGlue());
+
+        newGame.setAlignmentX(CENTER_ALIGNMENT);
+        loadGame.setAlignmentX(CENTER_ALIGNMENT);
+        quit.setAlignmentX(CENTER_ALIGNMENT);
+
+        newGame.addActionListener(e -> {
+            new CharacterCreation();
+            this.dispose();
+        });
+        loadGame.addActionListener(e -> {
+            new LoadScreen();
+            this.dispose();
+        });
+        quit.addActionListener(e -> {
+            System.exit(0);
+        });
+
+        this.getContentPane().add(start);
+        this.setSize(300, 300);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
     }
 }
