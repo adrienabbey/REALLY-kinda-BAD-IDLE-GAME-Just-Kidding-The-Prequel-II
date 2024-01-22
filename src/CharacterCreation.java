e15929cf38fba39715705fbb23d9cd24bf2b0252
rabiddoughnuts <111888263+rabiddoughnuts@users.noreply.github.com>
2024-01-22 12:20:07 -0500

    Buttons do things now

--- src/CharacterCreation.java
+++ src/CharacterCreation.java
@@ -4,16 +4,26 @@ import java.awt.GridLayout;
 import javax.swing.*;
 
 class CharacterCreation extends JFrame{
+
+    private int statPoints = 10;
+    private int muscle = 0;
+    private int brain = 0;
+    private int heart = 0;
+
     public CharacterCreation(){
-        int statPoints = 10;
+
         JPanel create = new JPanel(); // Create panel where you create your character
         create.setLayout(new BoxLayout(create, BoxLayout.Y_AXIS));
         // TODO - Make sure this is long enough to fit all the names we want to allow
         JTextField name = new JTextField(12);
         name.setMaximumSize(name.getPreferredSize());
-        JLabel message = new JLabel("You have :" + statPoints + " stat points left to spend.");
+        JLabel points = new JLabel("You have :" + statPoints + " stat points left to spend.");
+        JLabel message = new JLabel("");
+        JButton submit = new JButton("Submit");
         name.setAlignmentX(CENTER_ALIGNMENT);
+        points.setAlignmentX(CENTER_ALIGNMENT);
         message.setAlignmentX(CENTER_ALIGNMENT);
+        submit.setAlignmentX(CENTER_ALIGNMENT);
 
         JPanel stats = new JPanel(); // Stats panel where you select your stats
         stats.setLayout(new GridLayout(3, 2));
@@ -34,10 +44,81 @@ class CharacterCreation extends JFrame{
         create.add(Box.createVerticalGlue());
         create.add(name);
         create.add(Box.createRigidArea(new Dimension(0, 10)));
+        create.add(points);
+        create.add(Box.createRigidArea(new Dimension(0, 10)));
         create.add(message);
         create.add(Box.createRigidArea(new Dimension(0, 10)));
         create.add(stats);
+        create.add(Box.createRigidArea(new Dimension(0, 10)));
+        create.add(submit);
         create.add(Box.createVerticalGlue());
+
+        weaker.addActionListener(e -> {
+            if(muscle > 0){
+                muscle--;
+                statPoints++;
+                message.setText("You have :" + statPoints + " stat points left to spend.");
+                this.repaint();
+            } else {
+                message.setText("You cannot go below 0 in any stat.");
+                this.repaint();
+            }
+        });
+        stronger.addActionListener(e -> {
+            if(statPoints > 0){
+                muscle++;
+                statPoints--;
+                message.setText("You have :" + statPoints + " stat points left to spend.");
+                this.repaint();
+            } else {
+                message.setText("You do not have enough stat points to spend.");
+                this.repaint();
+            }
+        });
+        dumber.addActionListener(e -> {
+            if(brain > 0){
+                brain--;
+                statPoints++;
+                message.setText("You have :" + statPoints + " stat points left to spend.");
+                this.repaint();
+            } else {
+                message.setText("You cannot go below 0 in any stat.");
+                this.repaint();
+            }
+        });
+        smarter.addActionListener(e -> {
+            if(statPoints > 0){
+                brain++;
+                statPoints--;
+                message.setText("You have :" + statPoints + " stat points left to spend.");
+                this.repaint();
+            } else {
+                message.setText("You do not have enough stat points to spend.");
+                this.repaint();
+            }
+        });
+        softer.addActionListener(e -> {
+            if(heart > 0){
+                heart--;
+                statPoints++;
+                message.setText("You have :" + statPoints + " stat points left to spend.");
+                this.repaint();
+            } else {
+                message.setText("You cannot go below 0 in any stat.");
+                this.repaint();
+            }
+        });
+        tougher.addActionListener(e -> {
+            if(statPoints > 0){
+                heart++;
+                statPoints--;
+                message.setText("You have :" + statPoints + " stat points left to spend.");
+                this.repaint();
+            } else {
+                message.setText("You do not have enough stat points to spend.");
+                this.repaint();
+            }
+        });
 
         this.getContentPane().add(create);
         this.setSize(300, 300);
