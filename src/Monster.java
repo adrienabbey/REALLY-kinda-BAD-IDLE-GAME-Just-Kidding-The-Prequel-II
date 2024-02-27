d790f649ede9aaefd2324c0fd0e9f69257e61bae
Adrien Abbey <abbey.12@wright.edu>
2024-02-27 14:00:46 -0500

    very generic constructor, inheritance

--- src/Monster.java
+++ src/Monster.java
@@ -1,3 +1,17 @@
-class Monster{
-    
+/* 
+ * Player Class for REALLY (kinda) BAD IDLE GAME (Just Kidding) The Prequel II
+ * Adrien Abbey et al. Feb. 2024
+ */
+
+class Monster extends GameCharacter {
+
+    /* Constructors */
+
+    /**
+     * Generic constructor for Monsters. This class extends the GameCharacter
+     * class.
+     */
+    public Monster(String name, int muscle, int brain, int heart) {
+        super(name, muscle, brain, heart);
+    }
 }
