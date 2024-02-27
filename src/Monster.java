4ae5b38c4909c602f5291df546818354061dbc13
Adrien Abbey <abbey.12@wright.edu>
2024-02-27 15:22:07 -0500

    name != description

--- src/Monster.java
+++ src/Monster.java
@@ -37,19 +37,31 @@ class Monster extends GameCharacter {
 
         switch (name) {
             case PETROCK:
-                this.setName(
+                this.setName("Rabid Pet Rock");
+                this.setDescription(
                         "Someone glued googley eyes onto a pet rock, then abandoned it. Also, it has rabies and wants to kill you.");
                 this.setMuscle(2);
                 this.setBrain(2);
                 this.setHealth(2);
                 break;
             case HOBOGOBLIN:
-                this.setName(
-                        "Not a hobgoblin. A hobo goblin. This goblin is obviously suffering after becoming emancipated from from all its wealth, health, and home.  Also, it has googley eyes.");
+                this.setName("Hobo Goblin");
+                this.setDescription(
+                        "Commonly misidentified as a hobgoblin (so rude!), this hobo goblin is obviously suffering after becoming emancipated from from all its wealth, health, and home.  Also, it has googley eyes.");
                 this.setMuscle(4);
                 this.setBrain(4);
                 this.setHeart(4);
                 break;
         }
+    }
+
+    /* Functions */
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
     }
 }
