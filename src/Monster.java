6bc7ef48a103fbdae32afd88b1ac32c62b877fc0
Adrien Abbey <abbey.12@wright.edu>
2024-02-27 15:16:33 -0500

    Now with more sanity.  Also, googley eyes.

--- src/Monster.java
+++ src/Monster.java
@@ -5,7 +5,6 @@
 
 /*
  * TODO: Add an image field?
- * TODO: Add a text description field?
  */
 
 class Monster extends GameCharacter {
@@ -14,59 +13,43 @@ class Monster extends GameCharacter {
      * Predefined Monster name enum.
      */
     public enum MonsterName {
-        SEWERRAT,
-        WEAKGOBLIN,
-        STRONGGOBLIN,
-        GOBLINSHAMAN,
-        GOBLINKING
+        PETROCK,
+        HOBOGOBLIN
     }
 
-    /* Constructors */
+    /* Fields */
+    String description;
 
-    /**
-     * DO NOT USE THIS CONSTRUCTOR! YOU ARE A VERY BAD PERSON IF YOU DO!
-     */
-    public Monster(String name, int muscle, int brain, int heart) {
-        super(name, muscle, brain, heart);
-    }
+    /* Constructor */
 
     /**
      * The REAL Monster Constructor.
-     * Uses the MonsterName enum to constrict valid input.
+     * Uses the MonsterName enum to constrict valid inputs.
      * 
      * @param name Name of a monster from the MonsterName enum.
      * 
      */
     public Monster(MonsterName name) {
         // Create a "temporary" monster that we forcibly change (painfully):
-        super("FIXME", 999, 999, 999);
+        super("FIXME!", 999, 999, 999);
 
         // TODO: Balance these values.
-        if (name == MonsterName.SEWERRAT) {
-            this.setName("Sewer Rat");
-            this.setMuscle(1);
-            this.setBrain(1);
-            this.setHealth(1);
-        } else if (name == MonsterName.WEAKGOBLIN) {
-            this.setName("Weak Goblin");
-            this.setMuscle(2);
-            this.setBrain(2);
-            this.setHeart(2);
-        } else if (name == MonsterName.STRONGGOBLIN) {
-            this.setName("Strong Goblin");
-            this.setMuscle(4);
-            this.setBrain(2);
-            this.setHealth(4);
-        } else if (name == MonsterName.GOBLINSHAMAN) {
-            this.setName("Goblin Shaman");
-            this.setMuscle(3);
-            this.setBrain(6);
-            this.setHeart(4);
-        } else if (name == MonsterName.GOBLINKING) {
-            this.setName("The Goblin King");
-            this.setMuscle(10);
-            this.setBrain(10);
-            this.setHeart(10);
+
+        switch (name) {
+            case PETROCK:
+                this.setName(
+                        "Someone glued googley eyes onto a pet rock, then abandoned it. Also, it has rabies and wants to kill you.");
+                this.setMuscle(2);
+                this.setBrain(2);
+                this.setHealth(2);
+                break;
+            case HOBOGOBLIN:
+                this.setName(
+                        "Not a hobgoblin. A hobo goblin. This goblin is obviously suffering after becoming emancipated from from all its wealth, health, and home.  Also, it has googley eyes.");
+                this.setMuscle(4);
+                this.setBrain(4);
+                this.setHeart(4);
+                break;
         }
     }
 }
