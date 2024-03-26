c9e8664d1bea6bf027e6e3a6d5ab38f160f4233b
Adrien Abbey <abbey.12@wright.edu>
2024-03-26 18:27:26 -0400

    added boss flag, adjusted stats, added new boss

--- src/Monster.java
+++ src/Monster.java
@@ -14,12 +14,14 @@ class Monster extends GameCharacter {
      */
     public enum MonsterName {
         PETROCK,
-        HOBOGOBLIN
+        HOBOGOBLIN,
+        CYCLOPIAN_GENTLEMAN
     }
 
     /* Fields */
     private String description;
     private int goldRewarded;
+    private boolean isBoss;
 
     /* Constructor */
 
@@ -42,19 +44,30 @@ class Monster extends GameCharacter {
                 this.setDescription(
                         "Someone glued googley eyes onto a pet rock, then abandoned it. Also, it has rabies and wants to kill you.");
                 this.setMuscle(2);
-                this.setBrain(2);
-                this.setHealth(2);
+                this.setBrain(1);
+                this.setHealth(3);
                 this.goldRewarded = 2;
+                this.isBoss = false;
                 break;
             case HOBOGOBLIN:
                 this.setName("Hobo Goblin");
                 this.setDescription(
                         "Commonly misidentified as a hobgoblin (so rude!), this hobo goblin is obviously suffering after becoming emancipated from from all its wealth, health, and home.  Also, it has googley eyes.");
-                this.setMuscle(4);
-                this.setBrain(4);
-                this.setHeart(4);
+                this.setMuscle(6);
+                this.setBrain(2);
+                this.setHeart(2);
                 this.goldRewarded = 4;
+                this.isBoss = false;
                 break;
+            case CYCLOPIAN_GENTLEMAN:
+                this.setName("Cyclopian Gentleman");
+                this.setDescription(
+                        "This impecably dressed giant just radiates cultured sophistication.  He's even wearing a tophat with a monocle over his single, giant eye.  Unfortunately, you're too small to notice and he's about to step all over you.  Which is not to say he didn't actually see you, he just doesn't care.");
+                this.setMuscle(10);
+                this.setBrain(10);
+                this.setHeart(10);
+                this.goldRewarded = 20;
+                this.isBoss = true;
         }
     }
 
