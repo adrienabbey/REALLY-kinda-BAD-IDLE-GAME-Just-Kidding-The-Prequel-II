590c2445acc5c54c7b25cc51fdee84069afc5367
Adrien Abbey <abbey.12@wright.edu>
2024-03-28 18:34:15 -0400

    Autoformatted because I like making life harder for others while easier for myself.

--- src/Combat.java
+++ src/Combat.java
@@ -4,13 +4,14 @@ import javax.swing.JScrollPane;
 import javax.swing.BoxLayout;
 import javax.swing.Box;
 
-class Combat extends JPanel{
+class Combat extends JPanel {
     static Inventory inventory = Inventory.getInstance();
 
-    public enum MagicType{
+    public enum MagicType {
         ATTACK,
         HEAL
     }
+
     static PlayerCharacter player;
     static Monster enemy;
     static Dice dice = new Dice(20);
@@ -19,7 +20,7 @@ class Combat extends JPanel{
     static boolean combatActive = false;
     static JTextArea logs = new JTextArea("Combat Log Area\n", 25, 58);
 
-    public Combat(PlayerCharacter player, Monster enemy){
+    public Combat(PlayerCharacter player, Monster enemy) {
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         Combat.player = player;
         Combat.enemy = enemy;
@@ -29,7 +30,7 @@ class Combat extends JPanel{
         add(Box.createVerticalGlue());
     }
 
-    public static void addLog(String log){
+    public static void addLog(String log) {
         logs.append(log);
         logs.setCaretPosition(logs.getDocument().getLength());
     }
@@ -42,7 +43,7 @@ class Combat extends JPanel{
         }
     }
 
-    public static void startCombat(){
+    public static void startCombat() {
         combatActive = true;
         combatThread = new Thread(() -> {
             try {
@@ -54,28 +55,27 @@ class Combat extends JPanel{
         combatThread.start();
     }
 
-    public static void endCombat(){
+    public static void endCombat() {
         combatActive = false;
     }
 
-    public static void combatLoop() throws InterruptedException{
-        while(combatActive && player.getHealth() > 0){
-            if(enemy.getHealth() > 0){
+    public static void combatLoop() throws InterruptedException {
+        while (combatActive && player.getHealth() > 0) {
+            if (enemy.getHealth() > 0) {
                 // Player's turn
                 int playerRoll = dice.roll();
                 int enemyRoll = dice.roll();
-                if(playerRoll > enemyRoll){
+                if (playerRoll > enemyRoll) {
                     player.attack(enemy);
                     enemy.attack(player);
                 } else {
                     enemy.attack(player);
                     player.attack(enemy);
                 }
-                if(player.getMagic() > 0){
-                    if(magicType == MagicType.ATTACK){
+                if (player.getMagic() > 0) {
+                    if (magicType == MagicType.ATTACK) {
                         player.magicAttack(enemy);
-                    }
-                    else if(magicType == MagicType.HEAL){
+                    } else if (magicType == MagicType.HEAL) {
                         player.magicHeal();
                     }
                 } else {
@@ -83,14 +83,15 @@ class Combat extends JPanel{
                 }
                 Thread.sleep(1500);
             } else {
-                addLog(enemy.getName() + " has been defeated!\n" + player.getName() + " has gained " + enemy.getGoldReward() + " gold!\n");
+                addLog(enemy.getName() + " has been defeated!\n" + player.getName() + " has gained "
+                        + enemy.getGoldReward() + " gold!\n");
                 inventory.setGold(enemy.getGoldReward());// Update gold resource in inventory
 
                 enemy = Dungeon.getMonster();
             }
             Driver.charScreen.update();
         }
-        if(player.getHealth() <= 0){
+        if (player.getHealth() <= 0) {
             addLog(player.getName() + " has been defeated!\n");
             player.died();
         }
