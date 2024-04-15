5d74e02a07ad7d47ad503372f59899ec2009cccb
rabiddoughnuts <111888263+rabiddoughnuts@users.noreply.github.com>
2024-04-15 12:45:31 -0400

    Font scaling back button

--- src/Credits.java
+++ src/Credits.java
@@ -60,7 +60,7 @@ public class Credits extends JPanel {
         rollingText.setOpaque(true); // Make the background visible
 
         // Set the position and size of the 'Back' button
-        back.setBounds(10, 10, (screenSize.width / 15), (back.getFont().getSize() * 90));
+        back.setBounds(10, 10, (back.getFont().getSize() * 6), (screenSize.height / 24));
 
         // Set the position and size of the rolling text label
         rollingText.setBounds(xPos, yPos, (int)(screenSize.width / 2.1), (int)(rollingText.getFont().getSize() * 90));
