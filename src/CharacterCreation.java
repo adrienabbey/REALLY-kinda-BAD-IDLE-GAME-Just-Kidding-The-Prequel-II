218655c65087cb3cf3472b9715af66a09125e5dd
rabiddoughnuts <111888263+rabiddoughnuts@users.noreply.github.com>
2024-06-25 17:14:49 -0400

    Mess with layout management stuff, and code format.

--- src/CharacterCreation.java
+++ src/CharacterCreation.java
@@ -1,13 +1,27 @@
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
-import javax.swing.*;
 import java.util.ArrayList;
 import java.awt.Font;
 import java.awt.Color;
+import java.awt.BorderLayout;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
+import javax.swing.BoxLayout;
+import javax.swing.Box;
+
+        // JPanel rightPanel = new JPanel();
+        // rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.Y_AXIS));
+        // rightPanel.setAlignmentX(CENTER_ALIGNMENT);
+        // add(rightPanel);
 
 // This class manages the screen for creating a new character
 class CharacterCreation extends JPanel {
@@ -16,6 +30,18 @@ class CharacterCreation extends JPanel {
     private int muscle = 0;
     private int brain = 0;
     private int heart = 0;
+
+    // These are used for formating the gui elements
+    final private Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    final private int width = screenSize.width;
+    final private int height = screenSize.height;
+    final private Dimension BUTTON_SIZE = new Dimension(width / 18, height / 32);
+    final private Dimension BUTTON_GAP = new Dimension(0, height / 100);
+    final private int labelFont = width / 106;
+    final private int buttonFont = width / 80;
+
+    final int imageWidth = (int) (width / 4);
+    final int imageHeight = -1; // set to negative 1 to scale only one way
 
     /**
      * This function hosts the character creation screen
@@ -27,119 +53,165 @@ class CharacterCreation extends JPanel {
     public CharacterCreation() throws IOException {
 
         // This is all the physical gui elements and their properties
+        // this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        // this.setBackground(Color.LIGHT_GRAY); // sets color of background
 
+        JPanel namePanel = new JPanel();
+        namePanel.setLayout(new BoxLayout(namePanel, BoxLayout.X_AXIS));
         ArrayList<JLabel> labels = new ArrayList<JLabel>();
-        // TODO - Make sure this is long enough to fit all the names we want to allow
         JLabel nameLabel = new JLabel("Name");
         labels.add(nameLabel);
         JTextField name = new JTextField(12);
         name.setMaximumSize(name.getPreferredSize());
-        JLabel points = new JLabel("You have: " + statPoints + " stat points left to spend.");
+
+        namePanel.add(Box.createHorizontalGlue());
+        namePanel.add(nameLabel);
+        namePanel.add(Box.createHorizontalStrut(10));
+        namePanel.add(name);
+        namePanel.add(Box.createHorizontalGlue());
+
+        JPanel pointsPanel = new JPanel();
+        pointsPanel.setLayout(new BoxLayout(pointsPanel, BoxLayout.X_AXIS));
+        JLabel points = new JLabel("You have " + statPoints + " stat points left to spend.");
         labels.add(points);
-        JLabel message = new JLabel(" ");
-        labels.add(message);
-        JButton submit = new JButton("Submit");
-        nameLabel.setAlignmentX(CENTER_ALIGNMENT);
-        name.setAlignmentX(CENTER_ALIGNMENT);
-        points.setAlignmentX(CENTER_ALIGNMENT);
-        message.setAlignmentX(CENTER_ALIGNMENT);
-        submit.setAlignmentX(CENTER_ALIGNMENT);
+        // points.setBounds((width * (3/4)), ((height / 6) + (labelFont / 2)), points.getPreferredSize().width, points.getPreferredSize().height);
+        pointsPanel.add(Box.createHorizontalGlue());
+        pointsPanel.add(points);
+        pointsPanel.add(Box.createHorizontalGlue());
 
-        JPanel charPanel = new JPanel(); // Character panel where you see and set stats for your character
-        charPanel.setLayout(new BoxLayout(charPanel, BoxLayout.X_AXIS));
-        BufferedImage image = ImageIO.read(new File("assets/images/character12.png"));
-        JLabel pic = new JLabel(new ImageIcon(image));
+        JPanel splitPanel = new JPanel();
+        splitPanel.setLayout(new BoxLayout(splitPanel, BoxLayout.X_AXIS));
+        JPanel rightPanel = new JPanel();
+        JPanel leftPanel = new JPanel();
+        rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.Y_AXIS));
+        // leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
 
-        JPanel stats = new JPanel(); // Stats panel where you select your stats
-        stats.setLayout(new GridLayout(3, 3, 10, 10));
-        ArrayList<JButton> buttons = new ArrayList<JButton>();
+        splitPanel.add(leftPanel);
+        splitPanel.add(rightPanel);
+        
+        Image image = ImageIO.read(new File("assets/images/character12.png"));
+        Image scaledImage = image.getScaledInstance(imageWidth, imageHeight, Image.SCALE_SMOOTH);
+        ImageIcon imageIcon = new ImageIcon(scaledImage);
+        JLabel charImage = new JLabel(imageIcon);
+        // charImage.setAlignmentX(CENTER_ALIGNMENT);
+        // charImage.setBounds(width / 4, height / 2, scaledImage.getWidth(charImage), scaledImage.getHeight(charImage));
+        leftPanel.add(charImage);
+
         Color customColorBeige = new Color(253, 236, 166);
         Color customColorBrown = new Color(102, 72, 54);
         Color customColorBlue = new Color(46, 86, 161);
+
+        JLabel message = new JLabel(" ");
+        labels.add(message);
+        message.setBounds((width * (3/4)), ((height / 6) + (2*(labelFont / 2))), points.getPreferredSize().width, points.getPreferredSize().height);
+
+        ArrayList<JButton> buttons = new ArrayList<JButton>();
+
+        JPanel musclePanel = new JPanel();
+        musclePanel.setLayout(new BoxLayout(musclePanel, BoxLayout.X_AXIS));
+        JLabel muscleLabel = new JLabel("Muscle: " + muscle);
+        labels.add(muscleLabel);
+        muscleLabel.setBounds((width * (3/4)), ((height / 6) + (3*(labelFont / 2))), points.getPreferredSize().width, points.getPreferredSize().height);
         JButton weaker = new JButton("Weaker");
         buttons.add(weaker);
         JButton stronger = new JButton("Stronger");
         buttons.add(stronger);
-        JLabel muscleLabel = new JLabel("Muscle: " + muscle);
-        labels.add(muscleLabel);
+
+        musclePanel.add(Box.createHorizontalGlue());
+        musclePanel.add(weaker);
+        musclePanel.add(Box.createHorizontalStrut(10));
+        musclePanel.add(stronger);
+        musclePanel.add(Box.createHorizontalStrut(10));
+        musclePanel.add(muscleLabel);
+        musclePanel.add(Box.createHorizontalGlue());
+
+        JPanel brainPanel = new JPanel();
+        brainPanel.setLayout(new BoxLayout(brainPanel, BoxLayout.X_AXIS));
+        JLabel brainLabel = new JLabel("Brain: " + brain);
+        labels.add(brainLabel);
+        brainLabel.setBounds((width * (3/4)), ((height / 6) + (4*(labelFont / 2))), points.getPreferredSize().width, points.getPreferredSize().height);
         JButton dumber = new JButton("Dumber");
         buttons.add(dumber);
         JButton smarter = new JButton("Smarter");
         buttons.add(smarter);
-        JLabel brainLabel = new JLabel("Brain: " + brain);
-        labels.add(brainLabel);
+
+        brainPanel.add(Box.createHorizontalGlue());
+        brainPanel.add(dumber);
+        brainPanel.add(Box.createHorizontalStrut(10));
+        brainPanel.add(smarter);
+        brainPanel.add(Box.createHorizontalStrut(10));
+        brainPanel.add(brainLabel);
+        brainPanel.add(Box.createHorizontalGlue());
+
+        JPanel heartPanel = new JPanel();
+        heartPanel.setLayout(new BoxLayout(heartPanel, BoxLayout.X_AXIS));
+        JLabel heartLabel = new JLabel("Heart: " + heart);
+        labels.add(heartLabel);
+        heartLabel.setBounds((width * (3/4)), ((height / 6) + (5*(labelFont / 2))), points.getPreferredSize().width, points.getPreferredSize().height);
         JButton softer = new JButton("Softer");
         buttons.add(softer);
         JButton tougher = new JButton("Tougher");
         buttons.add(tougher);
-        JLabel heartLabel = new JLabel("Heart: " + heart);
-        labels.add(heartLabel);
 
-        // This section adds the components and controls layout
-        stats.add(weaker);
-        stats.add(stronger);
-        stats.add(muscleLabel);
-        stats.add(dumber);
-        stats.add(smarter);
-        stats.add(brainLabel);
-        stats.add(softer);
-        stats.add(tougher);
-        stats.add(heartLabel);
+        heartPanel.add(Box.createHorizontalGlue());
+        heartPanel.add(softer);
+        heartPanel.add(Box.createHorizontalStrut(10));
+        heartPanel.add(tougher);
+        heartPanel.add(Box.createHorizontalStrut(10));
+        heartPanel.add(heartLabel);
+        heartPanel.add(Box.createHorizontalGlue());
 
-        // format buttons
-        for (int i = 0; i < buttons.size(); i++) {
-            buttons.get(i).setAlignmentX(CENTER_ALIGNMENT);
-
-            buttons.get(i).setPreferredSize(new Dimension(200, 40));
-            buttons.get(i).setMaximumSize(new Dimension(200, 40));
-            buttons.get(i).setBackground(customColorBrown);
-            buttons.get(i).setForeground(customColorBeige);
-            buttons.get(i).setFont(new Font("Serif", Font.BOLD, 24));
-        }
-
+        JButton submit = new JButton("Submit");
         // submit button formating
+        submit.setAlignmentX(CENTER_ALIGNMENT);
         submit.setPreferredSize(new Dimension(200, 80));
         submit.setMaximumSize(new Dimension(200, 80));
         submit.setBackground(customColorBlue);
         submit.setForeground(Color.WHITE);
-        submit.setFont(new Font("Serif", Font.BOLD, 24));
+        submit.setFont(new Font("Serif", Font.BOLD, buttonFont));
+        submit.setBounds((width * (3/4)), ((height / 6) + (6*(labelFont / 2))), submit.getPreferredSize().width, submit.getPreferredSize().height);
+
+
+        rightPanel.add(Box.createVerticalGlue());
+        rightPanel.add(message);
+        rightPanel.add(Box.createVerticalStrut(10));
+        rightPanel.add(musclePanel);
+        rightPanel.add(Box.createVerticalStrut(10));
+        rightPanel.add(brainPanel);
+        rightPanel.add(Box.createVerticalStrut(10));
+        rightPanel.add(heartPanel);
+        rightPanel.add(Box.createVerticalStrut(10));
+        rightPanel.add(submit);
+        rightPanel.add(Box.createVerticalGlue());
+
+        this.add(Box.createVerticalGlue());
+        this.add(namePanel);
+        this.add(Box.createVerticalStrut(10));
+        this.add(pointsPanel);
+        this.add(Box.createVerticalStrut(10));
+        this.add(splitPanel);
+        // this.add(Box.createVerticalStrut(10));
+        // this.add(submit);
+        this.add(Box.createVerticalGlue());
+
 
         // Label formating
         for (int i = 0; i < labels.size(); i++) {
+            labels.get(i).setAlignmentX(CENTER_ALIGNMENT);
             labels.get(i).setForeground(customColorBrown);
-            labels.get(i).setFont(new Font("Serif", Font.BOLD, 18));
+            labels.get(i).setFont(new Font("Serif", Font.BOLD, labelFont));
         }
 
-        muscleLabel.setAlignmentX(RIGHT_ALIGNMENT);
-        brainLabel.setAlignmentX(RIGHT_ALIGNMENT);
-        heartLabel.setAlignmentX(RIGHT_ALIGNMENT);
-
-        charPanel.add(Box.createHorizontalGlue());
-        charPanel.add(pic);
-        charPanel.add(Box.createHorizontalGlue());
-        charPanel.add(stats);
-        charPanel.add(Box.createHorizontalGlue());
-        charPanel.add(submit);
-
-        add(Box.createVerticalGlue());
-        add(nameLabel);
-        add(name);
-        add(Box.createRigidArea(new Dimension(0, 5)));
-        add(points);
-        add(Box.createRigidArea(new Dimension(0, 5)));
-        add(message);
-        add(Box.createRigidArea(new Dimension(0, 5)));
-        add(charPanel);
-        add(Box.createRigidArea(new Dimension(0, 35)));
-        add(Box.createVerticalGlue());
-
-        nameLabel.setAlignmentX(CENTER_ALIGNMENT);
-        name.setAlignmentX(CENTER_ALIGNMENT);
-        points.setAlignmentX(CENTER_ALIGNMENT);
-        message.setAlignmentX(CENTER_ALIGNMENT);
-        submit.setAlignmentX(CENTER_ALIGNMENT);
+        // format buttons
+        for (int i = 0; i < buttons.size(); i++) {
+            buttons.get(i).setAlignmentX(CENTER_ALIGNMENT);
+            buttons.get(i).setPreferredSize(BUTTON_SIZE);
+            buttons.get(i).setMaximumSize(BUTTON_SIZE);
+            buttons.get(i).setBackground(customColorBrown);
+            buttons.get(i).setForeground(customColorBeige);
+            buttons.get(i).setFont(new Font("Serif", Font.BOLD, buttonFont));
+            buttons.get(i).setBounds((width * (3/4)), ((height / 6) + (6*(labelFont / 2)) + (i * (BUTTON_SIZE.height + BUTTON_GAP.height))), buttons.get(i).getPreferredSize().width, buttons.get(i).getPreferredSize().height);
+        }
 
         // This section allows the buttons to interact with the functions of the game
 
